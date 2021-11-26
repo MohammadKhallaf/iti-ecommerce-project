9e49efce87013185fd94713bbb58bc8f07ee3729
MohammedKhallaf <mohammed.khallaf.1997@gmail.com>
2021-11-26 12:51:42 +0200

    edit project structure

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+/*________PAGE-ELEMENTS________*/
+
+/*________GLOBAL-VARIABLES________*/
+
+/*________GLOBAL-FUNCTIONS________*/
+
+/*________HELPER-FUNCTIONS________*/
+
+/*________EVENTS________*/
