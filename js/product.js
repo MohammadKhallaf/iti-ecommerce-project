28004fdc940575793be227f1edeffdfcc3418981
MohammedKhallaf <mohammed.khallaf.1997@gmail.com>
2021-12-03 16:12:03 +0200

    update product page

--- js/product.js
+++ js/product.js
@@ -1,5 +1,14 @@
 "use strict";
-/*________PAGE-ELEMENTS________*/
+/*<<==========| PAGE-ELEMENTS |==========>>*/
+/*<-------summary__image__view----------------->*/
+const summaryIamgeView = document.getElementById("summary-img-container");
+const mainImageView = document.querySelector(".summary__img .large__img");
+const smallImagesContainer = document.querySelector(
+  ".summary__img .small__img__container"
+);
+const smallImages = document.querySelectorAll(".summary__img .small__img");
+const summaryRightArrow = document.querySelector(".summary__arrow_right");
+const summaryLeftArrow = document.querySelector(".summary__arrow_left");
 
 const addToCardButton = document.getElementById("add-to-cart");
 const addReviewButton = document.getElementById("add-review");
@@ -14,22 +23,63 @@ const descriptionTabButton = document.getElementById("description-tab-btn");
 const detailsTabButton = document.getElementById("details-tab-btn");
 const reviewsTabButton = document.getElementById("reviews-tab-btn");
 /*<-------details__tabs-------->*/
-
 const detailsSectionView = document.querySelector(".details__section");
-const descriptionView = detailsSectionView.querySelector(".details__description");
+const descriptionView = detailsSectionView.querySelector(
+  ".details__description"
+);
 const detailsView = detailsSectionView.querySelector(".details__details");
 const reviewsView = detailsSectionView.querySelector(".details__reviews");
 
-/*________GLOBAL-VARIABLES________*/
+/*<-------related_products__view----------------->*/
+const relatedScrollView = document.querySelector(".related__container__scroll");
+const relatedRightArrow = document.querySelector(".related__arrow_right");
+const relatedLeftArrow = document.querySelector(".related__arrow_left");
 
-/*========| GLOBAL-FUNCTIONS |========*/
-// ignore preittier
+/*<<==========| GLOABL-VARIABLES |==========>>*/
+const productImgSrc = [
+  "./res/img/product01.png",
+  "res/img/product03.png",
+  "res/img/product06.png",
+];
+let currentImgIndex = 0;
+
+mainImageView.src = productImgSrc[currentImgIndex];
+smallImages.forEach((element, index) => {
+  element.src = productImgSrc[index];
+});
+
+/*<<==========| GLOBAL-FUNCTIONS |==========>>*/
+
+/*<----------event handlers---------->*/
+
+/*<----------image of product------> */
+const imgRightArrowHandler = () => {
+  currentImgIndex++;
+  if (currentImgIndex > productImgSrc.length - 1) currentImgIndex = 0;
+  changeImage(currentImgIndex);
+};
+
+const imgLeftArrowHandler = () => {
+  currentImgIndex--;
+  if (currentImgIndex < 0) currentImgIndex = productImgSrc.length - 1;
+  changeImage(currentImgIndex);
+};
+
+const smallImagesContainerHandler = (e) => {
+  const images = [...e.target.parentElement.children];
+  console.log("Handler Clicked on index :", images.indexOf(e.target));
+  console.log("with pic :", productImgSrc[images.indexOf(e.target)]);
+  changeImage(images.indexOf(e.target));
+};
+
+/*<----------buttons of product------> */
 const addToCartHandler = (e) => {
   e.preventDefault();
 };
-
 const addReviewHandler = (e) => {
-  e.preventDefault();
+  reviewsTabHandler(e, true);
+  document.getElementById("review-form").scrollIntoView({ behavior: "smooth" });
+  // e.preventDefault();
 };
 const addWishHandler = (e) => {
   e.preventDefault();
@@ -51,38 +101,77 @@ const mailShare = (e) => {
 /* >----- details buttons ------< */
 const descriptionTabHandler = (e) => {
   e.preventDefault();
-  console.dir(e)
   descriptionView.classList.toggle("details--active");
-  for (const element of detailsSectionView.children){
-    if(element.classList.contains("details__description")) continue
-    element.classList.remove("details--active")
-
+  for (const element of detailsSectionView.children) {
+    if (element.classList.contains("details__description")) continue;
+    element.classList.remove("details--active");
+    reviewsView.classList.remove("details__reviews--active");
   }
-  
 };
 const detailsTabHandler = (e) => {
   e.preventDefault();
   detailsView.classList.toggle("details--active");
-  for (const element of detailsSectionView.children){
-    if(element.classList.contains("details__details")) continue
-    element.classList.remove("details--active")
-
+  for (const element of detailsSectionView.children) {
+    if (element.classList.contains("details__details")) continue;
+    element.classList.remove("details--active");
+    reviewsView.classList.remove("details__reviews--active");
   }
 };
-const reviewsTabHandler = (e) => {
+const reviewsTabHandler = (e, show) => {
   e.preventDefault();
-  reviewsView.classList.toggle("details__reviews--active");
-  for (const element of detailsSectionView.children){
-    if(element.classList.contains("details__reviews")) continue
-    element.classList.remove("details--active")
-
+  console.log(!!show);
+  if (!!show && reviewsView.classList.contains("details__reviews--active")) {
+  } else {
+    detailsView.classList.toggle("details--active");
+    reviewsView.classList.toggle("details__reviews--active");
+    for (const element of detailsSectionView.children) {
+      if (element.classList.contains("details__reviews")) continue;
+      element.classList.remove("details--active");
+    }
   }
 };
 
-/*________HELPER-FUNCTIONS________*/
+/*<----------related products------> */
+const rltdRightArrowhandler = (e) => {
+  let boundRe = relatedScrollView.getBoundingClientRect();
+  relatedScrollView.scrollBy(300, 0);
+  console.dir(boundRe);
+};
+const rltdLeftArrowhandler = () => {
+  let boundRe = relatedScrollView.getBoundingClientRect();
+  relatedScrollView.scrollBy(-300, 0);
+  console.dir(boundRe);
+};
 
+/*<<==========| HELPER-FUNCTIONS |==========>>*/
+
+const changeImage = (ImgIndex) => {
+  currentImgIndex = ImgIndex;
+  console.log("Before changing:", mainImageView.src, productImgSrc[ImgIndex]);
+  mainImageView.src = productImgSrc[ImgIndex];
+  console.log("After changing:", mainImageView.src, productImgSrc[ImgIndex]);
+  console.log("___________________________");
+
+  smallImages.forEach((element, index) => {
+    if (index === ImgIndex) {
+      smallImages[index].classList.add("small__img-active");
+    } else {
+      if (smallImages[index].classList.contains("small__img-active"))
+        smallImages[index].classList.toggle("small__img-active");
+    }
+  });
+};
 /*________EVENTS________*/
 
+/*<-large image-->*
+
+
+ *<-------------->*/
+summaryRightArrow.addEventListener("click", imgRightArrowHandler);
+summaryLeftArrow.addEventListener("click", imgLeftArrowHandler);
+/*<--------------> */
+smallImagesContainer.addEventListener("click", smallImagesContainerHandler);
+/*<--------------> */
 addToCardButton.addEventListener("click", addToCartHandler);
 addReviewButton.addEventListener("click", addReviewHandler);
 addWishButton.addEventListener("click", addWishHandler);
@@ -95,3 +184,6 @@ mailButton.addEventListener("click", mailShare);
 descriptionTabButton.addEventListener("click", descriptionTabHandler);
 detailsTabButton.addEventListener("click", detailsTabHandler);
 reviewsTabButton.addEventListener("click", reviewsTabHandler);
+/*<--------------->*/
+relatedRightArrow.addEventListener("click", rltdRightArrowhandler);
+relatedLeftArrow.addEventListener("click", rltdLeftArrowhandler);
